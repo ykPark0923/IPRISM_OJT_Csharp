5ffcfa79f0d2574bd278822ba8beb044594b11ff
ykPark <puklove22@naver.com>
2025-04-09 17:10:55 +0900

    배열 분할 주석추가

--- Array/Program.cs
+++ Array/Program.cs
@@ -13,6 +13,7 @@ namespace ArrayTest
 
         static void Main(string[] args)
         {
+            // 'Z' - 'A' + 1 = 90-65+1 = 26
             char[] array = new char['Z' - 'A' + 1];
             for (int i = 0; i < array.Length; i++)
             {
@@ -25,13 +26,14 @@ namespace ArrayTest
             Range range_5_10 = 5..10;
             PrintArray(array[range_5_10]);
 
+            //뒤에서 0번째, 즉 마지막까지
             Index last = ^0;
             Range range_5_last = 5..last;
 
             PrintArray(array[range_5_last]);
 
+            // 뒤에서 4번째부터 뒤에서 1번째까지
             PrintArray(array[^4..^1]);
-
         }
     }
 }
