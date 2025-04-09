1c76a09958735f3d128be62c30f2c8a6c7dc8248
ykPark <puklove22@naver.com>
2025-04-09 11:40:54 +0900

    initial commit

---
+++ ~Class/MainApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.Cache;
+
+namespace ConsoleApp1
+{    
+    abstract class AbstractBase
+    {
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA()
+        {
+            Console.WriteLine("AbstractBase,PublicMethosA()");
+        }
+
+        public abstract void AbstractMethodA();
+    }
+
+    class Derived : AbstractBase
+    {
+        public override void AbstractMethodA()
+        {
+            Console.WriteLine("Derived.AbstractMethodA()");
+
+            PrivateMethodA();
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMethodA();
+            obj.PublicMethodA();
+        }
+    }
+}
