ac3bc0ddd6fe01fcd80fb17a3a3a6e4940ceb38f
ykPark <puklove22@naver.com>
2025-04-09 15:28:19 +0900

    레코드 객체 비교하기

--- Property/MainApp.cs
+++ Property/MainApp.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Property
 {
-    record RTransaction
+    class CTransaction
     {
         public string From { get; init; }
         public string To { get; init; }
@@ -14,21 +15,64 @@ namespace Property
         }
     }
 
-        class MainApp
+    record RTransaction
+    {
+        public string From { get; init; }
+        public string To { get; init; }
+        public int Amount { get; init; }
+
+        public override string ToString()
+        {
+            return $"{From,-10}->{To,-10}: ${Amount}";
+        }
+    }
+
+    class MainApp
     {
         static void Main(string[] args)
         {
+            CTransaction trA = new CTransaction
+            {
+                From = "Alice",
+                To = "Bob",
+                Amount = 100
+            };
+
+            CTransaction trB = new CTransaction
+            {
+                From = "Alice",
+                To = "Bob",
+                Amount = 100
+            };
+
+            Console.WriteLine(trA);
+            Console.WriteLine(trB);
+
+
+            //기본 equals()의 기본 구현은 내용 비교가 아닌 참고를 비교하므로 false출력
+            Console.WriteLine($"{trA.Equals(trB)}");
+
+
+
+
+
             RTransaction tr1 = new RTransaction
             {
                 From="Alice", To="Bob", Amount=100
             };
 
-            RTransaction tr2 = tr1 with { To = "Charlie" };
-            RTransaction tr3 = tr2 with { From="Dave", Amount =30};
+            RTransaction tr2 = new RTransaction
+            {
+                From = "Alice",
+                To = "Bob",
+                Amount = 100
+            };
+
+
 
             Console.WriteLine(tr1);
             Console.WriteLine(tr2);
-            Console.WriteLine(tr3);
+            Console.WriteLine($"{tr1.Equals(tr2)}");
         }
     }
 }
