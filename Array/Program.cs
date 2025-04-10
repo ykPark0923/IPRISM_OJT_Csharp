06a2a6a94c6e408e1c84f3b8ecb06d41dc1da9f8
ykPark <puklove22@naver.com>
2025-04-11 08:59:19 +0900

    인덱서

--- Array/Program.cs
+++ Array/Program.cs
@@ -4,10 +4,51 @@ using System.Collections;
 
 namespace ArrayTest
 {
+    class MyList
+    {
+        private int[] array;
+
+        public MyList()
+        {
+            array = new int[3];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<int>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[index] = value;
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            MyList list = new MyList();
+            for (int i = 0; i < 5; i++)
+                list[i] = i;
+
+            for (int i = 0; i < list.Length; i++)
+                Console.WriteLine(list[i]);
             Hashtable ht = new Hashtable();
             ht["name"] = "Alice";
             ht["age"] = 25;
