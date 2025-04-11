67f1f1f166d93d35e3624b33d8e33c10e8f13122
ykPark <puklove22@naver.com>
2025-04-11 10:49:45 +0900

    예외 던지기

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -4,28 +4,28 @@ namespace Exception_
 {
     internal class Program
     {
-        static void DoSomething(int arg)
-        {
-            if (arg < 10)
-                Console.WriteLine($"arg : {arg}");
-            else
-                throw new Exception("arg가 10보다 큽니다.");
-        }
 
         static void Main(string[] args)
         {
             try
             {
-                DoSomething(1);
-                DoSomething(3);
-                DoSomething(5);
-                DoSomething(9);
-                DoSomething(11);
-                DoSomething(13);
+                int? a = null;
+                int b = a ?? throw new ArgumentNullException();
             }
-            catch (Exception e)
+            catch (ArgumentNullException e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                int[] array = new[] { 1, 2, 3 };
+                int index = 4;
+                int value = array[index >= 0 && index < 3 ? index : throw new IndexOutOfRangeException()];
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
