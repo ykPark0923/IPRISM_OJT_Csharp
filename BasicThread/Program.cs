02b297c9e248eb5e7ac9d87375fb25363c2194ee
ykPark <puklove22@naver.com>
2025-04-11 17:59:50 +0900

    스레드 임의종료

--- BasicThread/Program.cs
+++ BasicThread/Program.cs
@@ -3,39 +3,58 @@ using System.Threading; // Thread 클래스 사용을 위한 네임스페이스
 
 namespace BasicThread
 {
-    internal class Program
+    class SideTask
     {
-        // 쓰레드에서 실행할 메서드 정의
-        static void DoSomething()
+        int count;
+
+        public SideTask(int count)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine($"DoSomething : {i}"); // DoSomething 스레드의 작업 출력
-                Thread.Sleep(10); // 10ms 동안 일시 정지 (CPU 양보)
-            }
+            this.count = count;
         }
 
+        public void KeepAlive()
+        {
+            try
+            {
+                while (count > 0)
+                {
+                    Console.WriteLine($"{count--} left");
+                    Thread.Sleep(10);
+                    Console.WriteLine("Count : 0");
+                }
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine(e);
+                Thread.ResetAbort();
+            }
+            finally
+            {
+                Console.WriteLine("Clearning resource...");
+            }
+        }
+    }
+
+    internal class Program
+    {
         static void Main(string[] args)
         {
-            // 새로운 쓰레드 생성, 실행 메서드는 DoSomething
-            Thread t1 = new Thread(new ThreadStart(DoSomething));
-            // 위 구문은 아래와 같이 간단히 쓸 수도 있어요:
-            // Thread t1 = new Thread(DoSomething);
+            SideTask task = new SideTask(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
 
             Console.WriteLine("Starting thread...");
-            t1.Start(); // t1 쓰레드 시작 → DoSomething 실행됨
+            t1.Start();
 
-            // 메인 쓰레드에서 동시에 작업 수행
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine($"Main :{i}"); // 메인 스레드의 작업 출력
-                Thread.Sleep(10); // 10ms 쉬기 (스레드 간 번갈아 실행 보기 좋게 하기 위함)
-            }
+            Thread.Sleep(100);
+
+            Console.WriteLine("Aborting thread...");
+            t1.Abort();
 
             Console.WriteLine("Waiting until thread stops...");
-            t1.Join(); // t1 스레드가 끝날 때까지 Main 스레드 대기
+            t1.Join();
 
-            Console.WriteLine("Finished"); // 모든 스레드 종료 후 출력
+            Console.WriteLine("Finished");
         }
     }
 }
