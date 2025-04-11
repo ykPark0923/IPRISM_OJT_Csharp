8da9e24950388dce1da6897548e89b8877aed0e4
ykPark <puklove22@naver.com>
2025-04-11 09:18:54 +0900

    foreach가 가능한 객체 만들기

--- Array/Program.cs
+++ Array/Program.cs
@@ -4,38 +4,16 @@ using System.Collections;
 
 namespace ArrayTest
 {
-    class MyList
+    class MyEnumerator
     {
-        private int[] array;
-
-        public MyList()
+        int[] numbers = {1, 2, 3, 4};
+        public IEnumerator GetEnumerator()
         {
-            array = new int[3];
-        }
-
-        public int this[int index]
-        {
-            get
-            {
-                return array[index];
-            }
-
-            set
-            {
-                if (index >= array.Length)
-                {
-                    Array.Resize<int>(ref array, index + 1);
-                    Console.WriteLine($"Array Resized : {array.Length}");
-                }
-                array[index] = value;
-            }
-        }
-        public int Length
-        {
-            get
-            {
-                return array.Length;
-            }
+            yield return numbers[0];
+            yield return numbers[1];
+            yield return numbers[2];
+            yield break;
+            yield return numbers[3];
         }
     }
 
@@ -43,30 +21,10 @@ namespace ArrayTest
     {
         static void Main(string[] args)
         {
-            MyList list = new MyList();
-            for (int i = 0; i < 5; i++)
-                list[i] = i;
-
-            for (int i = 0; i < list.Length; i++)
-                Console.WriteLine(list[i]);
-            Hashtable ht = new Hashtable();
-            ht["name"] = "Alice";
-            ht["age"] = 25;
-
-            Console.WriteLine("Hashtable 출력:");
-            foreach (DictionaryEntry entry in ht)
+            var obj = new MyEnumerator();
+            foreach (int i in obj)
             {
-                Console.WriteLine($"{entry.Key}: {entry.Value}");
-            }
-
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-            dict["language"] = "C#";
-            dict["level"] = "Intermediate";
-
-            Console.WriteLine("Dictionary 출력:");
-            foreach (var pair in dict)
-            {
-                Console.WriteLine($"{pair.Key}: {pair.Value}");
+                Console.WriteLine(i);
             }
         }
     }
