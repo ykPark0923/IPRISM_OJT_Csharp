860b4ad37953dd9fbd2d57d831faf79c4b343069
ykPark <puklove22@naver.com>
2025-04-11 09:59:39 +0900

    예외처리

---
+++ Exception/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            try
+            {
+                for(int i=0; i<5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+            }
+
+            Console.WriteLine("종료");
+        }
+    }
+}
