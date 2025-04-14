856574106e77e5e8e7b2125a5869a5dc593c23f0
ykPark <puklove22@naver.com>
2025-04-14 14:08:58 +0900

    winform main form 생성

---
+++ 20_Winform/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Winform_20
+{
+
+    class MAinApp : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new MAinApp());
+        }
+    }
+}
+
