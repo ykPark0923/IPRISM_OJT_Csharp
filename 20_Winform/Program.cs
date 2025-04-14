ed2335c7ab3e7dfb94632fe699fe6f23ea6b0be4
ykPark <puklove22@naver.com>
2025-04-14 14:10:04 +0900

    form eventhandler delegate

--- 20_Winform/Program.cs
+++ 20_Winform/Program.cs
@@ -4,11 +4,22 @@
 namespace Winform_20
 {
 
-    class MAinApp : System.Windows.Forms.Form
+    class MyForm : System.Windows.Forms.Form
+    {
+
+    }
+    class MAinApp
     {
         static void Main(string[] args)
         {
-            System.Windows.Forms.Application.Run(new MAinApp());
+            MyForm form = new MyForm();
+            form.Click += new EventHandler(
+                (sender, eventArgs) =>
+                {
+                    Application.Exit();
+                });
+
+            Application.Run(form);
         }
     }
 }
