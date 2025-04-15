5f1a6f1824634bfc5f4466506e06b1f286c702db
ykPark <puklove22@naver.com>
2025-04-15 10:41:05 +0900

    delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate int MyDelegate(int a, int b);
+
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate Callback;
+
+            Callback = new MyDelegate(Calc.Plus);
+            Console.WriteLine(Callback(3, 4));
+
+            //Callback = new MyDelegate(Calculator.Minus);
+            Console.Write(Callback(7, 5));
+
+        }
+    }
+}
